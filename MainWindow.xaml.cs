ae9b846a8e0cc70dd15b4e7ad6e17e052e4ed3f4
Thomas <thomas.reyskens@gmail.com>
2014-06-09 03:26:24 +0200

    Server application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectSparkServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Server server;
+        public MainWindow()
+        {
+            InitializeComponent();
+            server = new Server();
+            server.serverResponse += getServerResponse;
+        }
+
+        private void getServerResponse(string message)
+        {
+            this.lbx_serverLog.Items.Add(message);
+        }
+
+        private void btn_ChangeStatus_Click(object sender, RoutedEventArgs e)
+        {
+            server.StartServer();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSparkServer
+{
+    public delegate void ServerResponse(string message);
+    class Server
+    {
+        private Socket _serverSocket;
+        public event ServerResponse serverResponse;
+        public Server()
+        {
+            this._serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //serverResponse("Server Started");
+        }
+
+        public void StartServer()
+        {
+            serverResponse("Server Started");
+        }
+    }
+}
